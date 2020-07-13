13b08fe9d742ca15fe400d1279ddc10811d2c7cd
kallard <kallard@student.21-school.ru>
2020-07-13 14:43:09 +0300

    Flag '0' is now ignored when flag '-' is present

--- parser/ft_flags_parse.c
+++ parser/ft_flags_parse.c
@@ -6,7 +6,7 @@
 /*   By: kallard <kallard@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/11 11:05:00 by kallard           #+#    #+#             */
-/*   Updated: 2020/07/13 14:00:40 by kallard          ###   ########.fr       */
+/*   Updated: 2020/07/13 14:40:50 by kallard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,12 @@ void		ft_flags_parse(const char* format, int*	i, t_format* argformat)
 		if (format[*i] == '-')
 		{
 			argformat->flags.minus = 1;
-			argformat->flags.zero = 0;
 			*i = *i + 1;
 		}
-		if (format[*i] == '0')
+		if (format[*i] == '0') //flag '0' is ignored when flag '-' is present
 		{
-			argformat->flags.zero = 1;
+			if (argformat->flags.minus = 0)
+				argformat->flags.zero = 1;
 			*i = *i + 1;
 		}
 	}
