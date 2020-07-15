974eebbb77f723a64a24f8cd7404961136a1ea34
kallard <kallard@student.21-school.ru>
2020-07-15 18:43:33 +0300

    Int type refactoring

--- processor/ft_int_type.c
+++ processor/ft_int_type.c
@@ -6,7 +6,7 @@
 /*   By: kallard <kallard@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/11 11:05:00 by kallard           #+#    #+#             */
-/*   Updated: 2020/07/15 16:24:15 by kallard          ###   ########.fr       */
+/*   Updated: 2020/07/15 18:41:57 by kallard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -92,7 +92,7 @@ static void ft_int_rightaligned(int arg, int dig, t_format* argformat)
 			else
 				while (n--)
 					write(1, " ", 1);
-			if (!(argformat->precision_is_present && argformat->precision == 0 && arg == 0)) //
+			if (dig)
 				ft_putnbr_fd(arg, 1);
 		}
 	}
