25a2c87d3366674af1a02c1c3993f7dc5146a394
kallard <kallard@student.21-school.ru>
2020-07-17 23:47:25 +0300

    ft_printf is adjusted to the Norm

--- base/ft_printf.c
+++ base/ft_printf.c
@@ -1,39 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kallard <kallard@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/17 23:25:09 by kallard           #+#    #+#             */
+/*   Updated: 2020/07/17 23:37:13 by kallard          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/ft_printf.h"
 #include "../includes/ft_parser.h"
 #include "../includes/ft_processor.h"
-# include <unistd.h>
-# include <stdarg.h>
+#include <unistd.h>
+#include <stdarg.h>
 
-int			ft_printf(const char* format, ...)
+int	ft_printf(const char *format, ...)
 {
-	int	i;
-	int c;
+	int			i;
+	int			count;
 	t_format	argformat;
 	t_ok		result;
+	va_list		argptr;
 
-	va_list argptr;
 	va_start(argptr, format);
-
-	c = 0;
+	count = 0;
 	i = 0;
-	while(format[i])
-	{
-		if(format[i] == '%')
+	while (format[i])
+		if (format[i] == '%')
 		{
 			i++;
 			argformat = ft_parser(&argptr, format, &i);
-			if ((result = ft_processor(&argptr, &argformat, &c)) == ERROR)
-				break;
+			if ((result = ft_processor(&argptr, &argformat, &count)) == ERROR)
+				break ;
 		}
 		else
 		{
 			write(1, &format[i++], 1);
-			c++;
+			count++;
 		}
-	}
 	va_end(argptr);
-	if(argformat.ok == ERROR || result == ERROR)
-		return (-1);
-	else
-		return (c);
+	return ((argformat.ok == ERROR || result == ERROR) ? -1 : count);
 }
