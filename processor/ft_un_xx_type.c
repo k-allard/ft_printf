3e0ce5051ff858b08e4e8fb3bacfcd18a1abc976
kallard <kallard@student.21-school.ru>
2020-07-17 23:12:27 +0300

    Refactoring. Everything works

--- processor/ft_un_xx_type.c
+++ processor/ft_un_xx_type.c
@@ -6,7 +6,7 @@
 /*   By: kallard <kallard@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/11 11:05:00 by kallard           #+#    #+#             */
-/*   Updated: 2020/07/17 22:25:01 by kallard          ###   ########.fr       */
+/*   Updated: 2020/07/17 22:55:54 by kallard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 
 static void ft_xx_leftaligned(char *arg, int symb, t_format* argformat, int *count)
 {
-	// int n;
-	
 	if (!argformat->precision_is_present || argformat->precision <= symb) //точность не дополняет нулями число
 	{
 		if (symb)
@@ -24,20 +22,16 @@ static void ft_xx_leftaligned(char *arg, int symb, t_format* argformat, int *cou
 			(*count) += symb;
 		}
 		writespaces(argformat->width - symb, count);
+		return ;
 	}
-	else //точность дополняет нулями число
-	{
-		writezeros(argformat->precision - symb, count);
-		ft_putstr_fd(arg, 1);
-		(*count) += symb;
-		writespaces(argformat->width - argformat->precision, count);
-	}
+	writezeros(argformat->precision - symb, count);
+	ft_putstr_fd(arg, 1);
+	(*count) += symb;
+	writespaces(argformat->width - argformat->precision, count);
 }
 
 static void ft_xx_rightaligned(char *arg, int symb, t_format* argformat, int *count)
 {
-	// int n;
-	
 	if (!argformat->precision_is_present || argformat->precision <= symb) //точность не дополняет нулями число
 		increase_to_width(argformat, argformat->width - symb, count);
 	else //точность дополняет нулями число
@@ -50,6 +44,20 @@ static void ft_xx_rightaligned(char *arg, int symb, t_format* argformat, int *co
 		ft_putstr_fd(arg, 1);
 		(*count) += symb;
 	}	
+}
+
+static t_ok no_width(char *print, int dig, t_format* argformat, int *count)
+{
+	if (dig > argformat->precision)	//число < точности -> точность тоже не нужна
+	{
+		ft_putstr_fd(print, 1);
+		(*count) += dig;
+		return OK;
+	}
+	writezeros(argformat->precision - dig, count);
+	ft_putstr_fd(print, 1);
+	(*count) += dig;
+	return OK;
 }
 
 t_ok		ft_un_xx_type(va_list* argptr, t_format* argformat, int *count)
@@ -68,25 +76,12 @@ t_ok		ft_un_xx_type(va_list* argptr, t_format* argformat, int *count)
 			print[n] = (char)ft_toupper((int)print[n]);
 	if (argformat->precision_is_present)
 	{
-		argformat->flags.zero = 0;   //Для типов d, i, o, u, x, X, если точность указана, флаг 0 игнорируется.
+		argformat->flags.zero = 0;
 		if (argformat->precision == 0 && arg == 0)
 			dig = 0;
 	}
-	if (argformat->width < argformat->precision || argformat->width < dig) //ширина и ее флаги не нужны
-	{
-		if (dig > argformat->precision)	//число < точности -> точность тоже не нужна
-		{
-			ft_putstr_fd(print, 1);
-			(*count) += dig;
-		}
-		else
-		{
-			writezeros(argformat->precision - dig, count);
-			ft_putstr_fd(print, 1);
-			(*count) += dig;
-		}
-		return OK;
-	}
+	if (argformat->width < argformat->precision || argformat->width < dig)
+		return(no_width(print, dig, argformat,count));
 	argformat->flags.minus ? ft_xx_leftaligned(print, dig, argformat,count) : ft_xx_rightaligned(print, dig, argformat, count);	
 	return OK;
 }
