273023e5897566784ca02bca54830fd1c4267b81
kallard <kallard@student.21-school.ru>
2020-07-18 00:02:34 +0300

    ft_parser.h and ft_printf.h are adjusted to the Norm

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: kallard <kallard@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/11 11:05:00 by kallard           #+#    #+#             */
-/*   Updated: 2020/07/11 11:11:18 by kallard          ###   ########.fr       */
+/*   Updated: 2020/07/17 23:49:01 by kallard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@
 # include <unistd.h>
 # include <stdarg.h>
 
-int 		ft_printf(const char * format, ...);
+int ft_printf(const char *format, ...);
 
 #endif
