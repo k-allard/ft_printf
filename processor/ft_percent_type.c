e16bd5b88d89556349d724206dd6605efa49494a
kallard <kallard@student.21-school.ru>
2020-07-17 23:45:44 +0300

    Type % now counts printed symbols

--- processor/ft_percent_type.c
+++ processor/ft_percent_type.c
@@ -6,41 +6,26 @@
 /*   By: kallard <kallard@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/11 11:05:00 by kallard           #+#    #+#             */
-/*   Updated: 2020/07/16 21:39:27 by kallard          ###   ########.fr       */
+/*   Updated: 2020/07/17 23:17:46 by kallard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_processor.h"
 
 t_ok		ft_percent_type(t_format* argformat, int* count)
-{
-	*count = *count + 0;
-	
+{	
 	if (argformat->width <= 1) //ширина и флаги выравнивания не будут нужны
 		write(1, "%", 1);
-	
 	else if (argformat->flags.minus)
 	{
 		write(1, "%", 1);
-		while (--argformat->width)
-		{
-			if (argformat->flags.zero)
-				write(1, "0", 1);
-			else
-				write(1, " ", 1);
-		}
+		increase_to_width(argformat, argformat->width - 1, count);
 	}
 	else
 	{
-		while (--argformat->width)
-		{
-			if (argformat->flags.zero)
-				write(1, "0", 1);
-			else
-				write(1, " ", 1);
-		}
+		increase_to_width(argformat, argformat->width - 1, count);
 		write(1, "%", 1);
 	}
-
+	(*count)++;
 	return OK;
 }
