06238aea8be427a3125f04e95742733d2e0e6d30
kallard <kallard@student.21-school.ru>
2020-07-18 12:51:53 +0300

    ft_width_parse.c is Norm

--- parser/ft_width_parse.c
+++ parser/ft_width_parse.c
@@ -6,40 +6,42 @@
 /*   By: kallard <kallard@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/11 11:05:00 by kallard           #+#    #+#             */
-/*   Updated: 2020/07/18 00:13:38 by kallard          ###   ########.fr       */
+/*   Updated: 2020/07/18 12:50:38 by kallard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_parser.h"
 
-void ft_width_parse(va_list *argptr, const char *format, int *i, t_format *argformat)
-{	
-	char *f;
-	int n;
+static void	basic_width_parse(const char *format, int *i, t_format *argformat)
+{
+	char	*f;
+	int		n;
+
+	f = (char *)format;
+	argformat->width = ft_atoi(&(f[*i]));
+	n = argformat->width;
+	while (n)
+	{
+		n /= 10;
+		(*i)++;
+	}
+}
+
+void		ft_width_parse(va_list *argptr, const char *format, int *i, \
+			t_format *argformat)
+{
 	argformat->width = 0;
 	if (format[*i] == '*')
 	{
 		argformat->width = va_arg(*argptr, int);
+		(*i)++;
 		if (argformat->width < 0)
 		{
 			argformat->flags.minus = 1;
 			argformat->flags.zero = 0;
 			argformat->width *= -1;
 		}
-		(*i)++;
-		return ;
 	}
-
 	else if (ft_isdigit(format[*i]))
-	{
-		f = (char *)format;
-		argformat->width = ft_atoi(&(f[*i]));
-	}
-		
-	n = argformat->width;
-    while (n) 
-	{
-        n/=10;
-        (*i)++; 
-	}
+		basic_width_parse(format, i, argformat);
 }
