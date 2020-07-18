b71838a477fe3bb8ce831f292eead010d2603d58
kallard <kallard@student.21-school.ru>
2020-07-18 12:43:35 +0300

    ft_prec_parse.c is Norm

--- parser/ft_prec_parse.c
+++ parser/ft_prec_parse.c
@@ -6,18 +6,32 @@
 /*   By: kallard <kallard@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/11 11:05:00 by kallard           #+#    #+#             */
-/*   Updated: 2020/07/18 00:47:46 by kallard          ###   ########.fr       */
+/*   Updated: 2020/07/18 12:43:11 by kallard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_parser.h"
 
-void	ft_prec_parse(va_list *argptr, const char *format, int *i, \
-		t_format *argformat)
+static void	basic_prec_parse(const char *format, int *i, t_format *argformat)
 {
-	int n;
-	char *f;
+	char	*f;
+	int		n;
 
+	while (format[*i] == '0')
+		(*i)++;
+	f = (char *)format;
+	argformat->precision = ft_atoi(&(f[*i]));
+	n = argformat->precision;
+	while (n)
+	{
+		n /= 10;
+		(*i)++;
+	}
+}
+
+void		ft_prec_parse(va_list *argptr, const char *format, int *i, \
+			t_format *argformat)
+{
 	argformat->precision_is_present = 0;
 	argformat->precision = 0;
 	if (format[*i] == '.')
@@ -25,28 +39,15 @@ void	ft_prec_parse(va_list *argptr, const char *format, int *i, \
 		argformat->precision_is_present = 1;
 		(*i)++;
 	}
-	else return ;
+	else
+		return ;
 	if (format[*i] == '*')
 	{
 		argformat->precision = va_arg(*argptr, int);
+		(*i)++;
 		if (argformat->precision < 0)
-		{
 			argformat->precision_is_present = 0;
-			(*i)++;
-			return ;
-		}
 	}
 	else if (ft_isdigit(format[*i]))
-	{
-		while (format[*i] == '0')
-			(*i)++;
-		f = (char *)format;
-		argformat->precision = ft_atoi(&(f[*i]));
-	}
-	n = argformat->precision;
-    while (n) 
-	{
-        n/=10;
-        (*i)++; 
-	}
+		basic_prec_parse(format, i, argformat);
 }
