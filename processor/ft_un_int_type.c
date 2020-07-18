4b22969ba740a7d3a157471c7455476d83658181
kallard <kallard@student.21-school.ru>
2020-07-18 14:01:01 +0300

    Adjusting to the Norm

--- processor/ft_un_int_type.c
+++ processor/ft_un_int_type.c
@@ -6,15 +6,15 @@
 /*   By: kallard <kallard@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/11 11:05:00 by kallard           #+#    #+#             */
-/*   Updated: 2020/07/18 13:34:47 by kallard          ###   ########.fr       */
+/*   Updated: 2020/07/18 14:00:27 by kallard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_processor.h"
 
-static void ft_un_int_leftaligned(unsigned int arg, int dig, t_format* argformat, int *count)
-{	
-	if (dig >= argformat->prec) //если число >= точности, точность значения не имеет
+static void	u_left(unsigned int arg, int dig, t_format *argformat, int *count)
+{
+	if (dig >= argformat->prec)
 	{
 		if (dig)
 		{
@@ -30,9 +30,9 @@ static void ft_un_int_leftaligned(unsigned int arg, int dig, t_format* argformat
 	width_increase(argformat, argformat->width - argformat->prec, count);
 }
 
-static void ft_un_int_rightaligned(unsigned int arg, int dig, t_format* argformat, int *count)
+static void	u_right(unsigned int arg, int dig, t_format *argformat, int *count)
 {
-	if (dig >= argformat->prec) //если число >= точности, точность значения не имеет
+	if (dig >= argformat->prec)
 	{
 		width_increase(argformat, argformat->width - dig, count);
 		if (dig)
@@ -51,27 +51,28 @@ static void ft_un_int_rightaligned(unsigned int arg, int dig, t_format* argforma
 	}
 }
 
-t_ok		ft_un_int_type(va_list* argptr, t_format* argformat, int *count)
+t_ok		ft_un_int_type(va_list *argptr, t_format *argformat, int *count)
 {
-	unsigned int arg;
-	int dig;
-	
+	unsigned int	arg;
+	int				dig;
+
 	arg = (unsigned int)va_arg(*argptr, unsigned int);
 	dig = digcount(arg);
 	if (argformat->prec_is_present)
 	{
-		argformat->flags.zero = 0;   //Для типов d, i, o, u, x, X, если точность указана, флаг 0 игнорируется.
+		argformat->flags.zero = 0;
 		if (argformat->prec == 0 && arg == 0)
 			dig = 0;
 	}
-	if (argformat->width < argformat->prec || argformat->width < dig) //случаи когда ширина и флаги выравнивания не нужны
+	if (argformat->width < argformat->prec || argformat->width < dig)
 	{
-		if (dig <= argformat->prec)	//число < точности -> точность тоже не нужна
+		if (dig <= argformat->prec)
 			writezeros(argformat->prec - dig, count);
 		ft_putstr_fd(ft_itoa_convert(arg, 10), 1);
 		(*count) += dig;
-		return OK;
+		return (OK);
 	}
-	argformat->flags.minus ? ft_un_int_leftaligned(arg, dig, argformat, count) : ft_un_int_rightaligned(arg, dig, argformat, count);	
-	return OK;
+	argformat->flags.minus ? u_left(arg, dig, argformat, count) :
+							u_right(arg, dig, argformat, count);
+	return (OK);
 }
